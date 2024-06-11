be0e19e12009a27695679da2e7e5663ac4ac5611
Gerake wk <gerake.wk@outlook.com>
2024-06-11 19:14:12 -0300

    Socket io adiocionado em outro componente

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import './App.css';
-import WindowComponent from './components/WindowComponent'
-
-
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
+import OperadoresLogados from './components/SocketIo'; 
 
 
 function App() {
 
-    const [number, setNumber] = useState(null)
-
-    useEffect(() => {
-        const socket = io ('http://172.20.20.49:5020')
-
-        socket.on('connect', () => {
-            console.log('Conectado ao servidor')
-        })
-
-        socket.on('operadores_logados', (data) => {
-            // console.log(`Operadores Logados: ${data.operadores_logados}`)
-            setNumber(data.operadores_logados) // atualiza com o número recebido
-        })
-
-        return () => socket.disconnect()
-    }, [])
-
     return(
         <div className='App'>
-            <h1>Teste de conexão Socket IO</h1>
-            <p>Operadores Logados: {number}</p>
-            <WindowComponent />
+            <OperadoresLogados />
         </div>
         
     )
--- src/components/WindowComponent.js
+++ src/components/WindowComponent.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function WindowComponent() {
+
+    const qtdNumOperador = document.getElementById('qtdNumOperador').innerText
+
     return (
         <div className="App">
             <h2>Teste window</h2>
+            <p>{qtdNumOperador}</p>
         </div>
     )
 }
