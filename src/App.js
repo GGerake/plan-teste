1312df80de895b755b78de8422c187d3d611daf4
Gerake <gabrielgerake@outlook.com>
2024-06-11 22:34:51 -0300

    ajuste function App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+
 import './App.css';
 import GraficoComponent from './components/GraficoComponent';
-import OperadoresLogados from './components/SocketIo'; 
+// import OperadoresLogados from './components/SocketIo'; 
 
 
 function App() {
+  const [theme, colorMode] = useMode();
 
-    return(
-        <div className='App'>
-            <OperadoresLogados />
-            <p>Teste local</p>
-            <GraficoComponent />
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <main className="content">
+            <Topbar/>
+          </main>
+
+          {/* <OperadoresLogados /> */}
+          <GraficoComponent />
+
         </div>
-        
-    )
-};
-
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
 export default App;
+
+// Bibliotecas instaladas no início
+// npm i @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar@0.7.1 formik yup @fullcalendar/core @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo
